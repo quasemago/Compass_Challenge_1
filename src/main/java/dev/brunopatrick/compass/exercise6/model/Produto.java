014946061ad85d2ccef7f4cc3f77dc773cd8542c
quasemago <brunoronningfn@gmail.com>
2024-01-02 17:54:01 -0400

    Exercício 6: Criado a classe entidade "Produto" seguindo os requisitos

---
+++ src/main/java/dev/brunopatrick/compass/exercise6/model/Produto.java
@@ -0,0 +1,65 @@
+package dev.brunopatrick.compass.exercise6.model;
+
+import java.util.UUID;
+
+public class Produto {
+    private String nome;
+    private UUID codigo;
+    private Double preco;
+    private Integer quantidadeEstoque;
+
+    public Produto(String nome, UUID codigo, Double preco, Integer quantidadeEstoque) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public UUID getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(UUID codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void comprar(Integer quantidade) {
+        this.quantidadeEstoque += quantidade;
+    }
+
+    public void vender(Integer quantidade) {
+        this.quantidadeEstoque -= quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + getNome() + "\n" +
+                "Código: " + getCodigo() + "\n" +
+                "Preço: " + String.format("%.2f", getPreco()) + "\n" +
+                "Quantidade em Estoque: " + getQuantidadeEstoque();
+    }
+}
