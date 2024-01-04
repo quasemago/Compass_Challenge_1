907e66370ba7af5cbe811783c8afc389e9c57673
quasemago <brunoronningfn@gmail.com>
2024-01-04 18:04:17 -0400

    Exercício 1: Faz tratamento de exceção.

---
+++ src/main/java/dev/brunopatrick/compass/exercise1/exception/QuizException.java
@@ -0,0 +1,7 @@
+package dev.brunopatrick.compass.exercise1.exception;
+
+public class QuizException extends RuntimeException {
+    public QuizException(String message) {
+        super(message);
+    }
+}
